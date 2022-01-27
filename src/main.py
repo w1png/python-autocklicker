2cbd634611b0ae0949f308aa74687899d24d2f8b
w1png <w1png@yandex.ru>
2022-01-27 16:56:30 +0300

    Add --hold functionality

--- src/main.py
+++ src/main.py
@@ -7,16 +7,25 @@ MOUSE = Controller()
 CPS = 10
 MAX_CPS = 40
 KEY = "x"
+HOLD = False
 
 def click(button=Button.left):
     MOUSE.press(button)
     sleep(0.01)
     MOUSE.release(button)
 
+def hold_handler(held, button=Button.left):
+    if held:
+        MOUSE.press(button)
+    else:
+        MOUSE.release(button)
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-c", "--cps", help=f"autoclicker's cps. Max cps is {MAX_CPS}. Default cps is {CPS}.")
     parser.add_argument("-k", "--key", help=f"autoclicker's start key. Default key is \"{KEY}\".")
+    parser.add_argument("--hold", help="[True/False] hold on keystroke.")
 
     # Setting cps
     if parser.parse_args().cps:
@@ -27,26 +36,40 @@ if __name__ == "__main__":
             CPS = 1
         else:
             CPS = int(cps_raw)
+    
+    if parser.parse_args().key:
+        KEY = parser.parse_args().key
+    
+    if parser.parse_args().hold:
+        match parser.parse_args().hold.lower():
+            case "true":
+                HOLD = True
+            case "false":
+                HOLD = False
 
-    print(f"CPS: {CPS}\nPress \"{KEY}\" to activate/stop the autoclicker.")
+    print((f"CPS: {CPS}\n" if not HOLD else "") + f"Press \"{KEY}\" to activate/stop the autoclicker.")
 
     # Actual autoclicker
     active = False
     last_press = time()
+    held = False
 
     while True:
         try:
             if keyboard.is_pressed(KEY) and time() > last_press+1: # Only register keystrocke a second after the last one
                 last_press = time()
                 active = not active
+                held = active
                 print("Activated." if active else "Stopped.")
+            if active:
+                if HOLD:
+                    hold_handler(held)
+                else:
+                    click()
+                    sleep(1./CPS-(1./CPS)*(CPS/100)) # it's still not really precise but it is close enough.
         except KeyboardInterrupt:
             print("\nStopped.")
             exit(0)
         finally:
             pass # Had to do this because it throws an error evey time the key is not pressed. A really poor design choice by the developers of the keyboard module imo.
-
-        if active:
-            click()
-            sleep(1./CPS-(1./CPS)*(CPS/100)) # it's still not really precise but it is close enough.
             
